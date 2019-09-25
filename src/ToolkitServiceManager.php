25c6b166ba5dc6051696be37e983bcc6ac6e2f90
wjonkerhulst <54948500+wjonkerhulst@users.noreply.github.com>
2019-09-25 08:19:31 +0200

    Laravel 6 Fix
    https://stackoverflow.com/questions/57884285/laravel-6-call-to-undefined-function-facuz-theme-array-get

--- src/ToolkitServiceManager.php
+++ src/ToolkitServiceManager.php
@@ -99,7 +99,7 @@ class ToolkitServiceManager
         // If the configuration doesn't exist, we'll throw an exception and bail.
         $connections = config('database.connections');
 
-        if (is_null($config = array_get($connections, $name))) {
+        if (is_null($config = Arr::get($connections, $name))) {
             throw new \InvalidArgumentException("Database [$name] not configured.");
         }
 
